000c51859106de14163e5e46a3b7e174fa07b8c9
Juli Romero <juliiromero2@gmail.com>
2021-09-03 20:12:44 -0300

    agrego icon al toast

--- src/app/components/shared/toast/toast.component.ts
+++ src/app/components/shared/toast/toast.component.ts
@@ -3,22 +3,8 @@ import { ToastServiceService } from '../../../services/toast-service.service'
 
 @Component({
   selector: 'app-toast',
-  template: `
-    <ngb-toast
-      *ngFor="let toast of toastService.toasts"
-      [header]="toast.headertext"
-      [class]="toast.classname"
-      [autohide]="toast.autohide"
-      [delay]="toast.delay || 5000"
-      (hide)="toastService.remove(toast)"
-    >
-      <ng-template [ngIf]="isTemplate(toast)" [ngIfElse]="text">
-        <ng-template [ngTemplateOutlet]="toast.textOrTpl"></ng-template>
-      </ng-template>
-
-      <ng-template #text>{{ toast.textOrTpl }}</ng-template>
-    </ngb-toast>
-  `,
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.css'],
   host: {'[class.ngb-toasts]': 'true'}
 })
 export class ToastComponent {
@@ -26,5 +12,20 @@ export class ToastComponent {
   constructor(public toastService: ToastServiceService) {}
 
   isTemplate(toast) { return toast.textOrTpl instanceof TemplateRef; }
+  // `
+  //   <ngb-toast
+  //     *ngFor="let toast of toastService.toasts"
+  //     [header]="toast.headertext"
+  //     [class]="toast.classname"
+  //     [autohide]="toast.autohide"
+  //     [delay]="toast.delay || 5000"
+  //     (hide)="toastService.remove(toast)"
+  //   >
+  //     <ng-template [ngIf]="isTemplate(toast)" [ngIfElse]="text">
+  //       <ng-template [ngTemplateOutlet]="toast.textOrTpl"></ng-template>
+  //     </ng-template>
 
+  //     <ng-template #text>{{ toast.textOrTpl }}</ng-template>
+  //   </ngb-toast>
+  // `
 }
